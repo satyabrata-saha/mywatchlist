8b2098439b4744b7acddf79c6f3b0c7e13348908
Satyabrata Saha <108542741+satyabrata-saha@users.noreply.github.com>
2025-03-16 23:37:43 +0530

    "Update Card component to handle missing links "

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -39,7 +39,7 @@ const Card = ({
   return (
     <div className="flex flex-col justify-start items-center overflow-hidden sm:w-full min-h-full border border-slate-100/5 p-1 sm:p-2 rounded-lg shadow-xl hover:scale-[100.5%] transition-all duration-300 ease-in-out">
       <div className="w-full h-full flex flex-col justify-between">
-        <Link href={link} className="w-full h-fit" target="_blank">
+        <Link href={link || "#"} className="w-full h-fit" target="_blank">
           <div className="h-full flex items-center justify-center">
             <img
               src={thumbnail || "/placeholder.png"}
@@ -51,7 +51,7 @@ const Card = ({
           </div>
         </Link>
         <div className="flex flex-col items-baseline justify-center gap-2 w-full h-full">
-          <Link href={link} className="w-full h-full">
+          <Link href={link || "#"} className="w-full h-full">
             <h5 className="text-sm sm:text-base text-blue-200/75 font-semibold mt-4 pb-2 text-wrap hover:text-blue-300 transition-all duration-200 ease-in-out">
               <span className="block">{title}</span>
               <span className="text-slate-100/50 text-xs sm:text-sm">
@@ -73,7 +73,7 @@ const Card = ({
             <span className="text-slate-100/75">Genres: </span>
 
             <span className="flex flex-wrap gap-1 mt-1 tracking-wide">
-              {genres.split(", ").map((genre, index) => (
+              {genres?.split(", ").map((genre, index) => (
                 <span
                   key={index}
                   onClick={() => genresSearch(genre)}
